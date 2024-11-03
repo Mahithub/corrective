9c10ac9229a8e2253a0f060bc9c00c337b3401a9
Mahithub <recappingmovie@gmail.com>
2024-11-03 11:46:57 +0300

    final push

--- style.js
+++ style.js
@@ -1,5 +1,5 @@
 function getting(response) {
-    console.log(response.data);
+
     let type = document.querySelector("#type");
     type.innerHTML = response.data.condition.description;
     let humidity = document.querySelector("#humidity");
@@ -11,26 +11,31 @@ function getting(response) {
     temperaturCity.innerHTML = `${Math.round(response.data.temperature.current)}`;
     let heading = document.querySelector("h1");
     heading.innerHTML = response.data.city;
-    let now = new Date();
-    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = weekDays[now.getDay()];
-    let getDay = document.querySelector("#day");
-    getDay.innerHTML = day;
-    let hour = now.getHours();
-    let getHour = document.querySelector("#hour");
-    getHour.innerHTML = hour;
-    let minute = now.getMinutes();
-    let getminute = document.querySelector("#minute");
-    getminute.innerHTML = minute;
-
     let icon = document.querySelector("#icoon");
     icon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="" class="no1">`;
-    
+    dailyForcast(response.data.city);
+}
+
+function todayDate(date) {
+    let day = date.getDay();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let newDate = weekDays[day];
+
     if (minute < 10) {
         minute = `0${minute}`;
     }
+    if (hour < 10) {
+        hour = `0${hour}`;
+    }
+    return `${newDate} ${hour}:${minute},`
+} 
+let currentDateElement = document.querySelector("#day");
+let currentDate = new Date();
+currentDateElement.innerHTML = todayDate(currentDate);
 
-}
 function searchcity(city) {
     let apikey = "795fc3dt59e943954b5e5aoc405fb9c4";
     let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&unit='metrics`;
@@ -42,5 +47,31 @@ function valuing(event) {
     searchcity(forming.value);
 }
 let searching = document.querySelector("#search-element");
-searching.addEventListener("submit", valuing)
+searching.addEventListener("submit", valuing);
 
+function goSearching(response) {
+
+    console.log(response.data.daily);
+    let forcastHTML = "";
+    let weatherforcast = document.querySelector("#weather-forcast");
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    response.data.daily.forEach(function (day, index) {
+            if (index < 5) {
+                let date = new Date(day.time * 1000);
+                let dayName = days[date.getDay()];
+                forcastHTML += `<div class="weather-forcast">
+        <div id="class-one">
+            <div id="weather-forcast-date">${dayName}</div>
+            <div><img src="${day.condition.icon_url}" /></div>
+            <div id="weather-forcast-temperature"><strong>${Math.round(day.temperature.maximum)}° </strong> ${Math.round(day.temperature.minimum)}°</div>
+        </div>
+    </div>`;
+            }
+            });
+        weatherforcast.innerHTML = forcastHTML;
+    };
+function dailyForcast(city) {
+    let apikey = "795fc3dt59e943954b5e5aoc405fb9c4";
+    let apiurl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}&unit='metric`;
+    axios.get(apiurl).then(goSearching);
+};
